23432a8b11bfff759b2621af4ad07c3c077682bb
kerokenji <nhockzukon9x@gmail.com>
2025-10-28 17:00:23 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = "tinh-ngay-tot-v1";
-const FILES_TO_CACHE = [
+// Tên cache
+const CACHE_NAME = "ngaytot-cache-v1";
+
+// Các file cần cache để chạy offline
+const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+// Cài đặt cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Kích hoạt và dọn cache cũ
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Chặn yêu cầu để dùng cache offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
